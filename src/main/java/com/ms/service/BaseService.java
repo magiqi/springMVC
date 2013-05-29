b5bada373c3cb5b949b93d81ce81ad1fd7913d3c
magi <xf.q@qq.com>
2013-05-29 23:30:07 +0800

    1.没有aop到的版本

--- src/main/java/com/ms/service/BaseService.java
+++ src/main/java/com/ms/service/BaseService.java
@@ -3,9 +3,10 @@ package com.ms.service;
 import com.ms.util.ApplicationContextUtil;
 
 public class BaseService {
-	 
+
 	public void save(Object o) {
 		ApplicationContextUtil.getSession().save(o);
+		int a = 21 / 0;
 	}
 
 }
